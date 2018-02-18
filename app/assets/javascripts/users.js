ffb8d6e62de286cef9be189e031d9fe4dbd1cd45
majesty86 <tladislas@gmail.com>
2018-02-17 23:20:50 -0600

    Add contact list, play with JS a bit - need AJAX for jQuery'd links as far as I know.

---
+++ app/assets/javascripts/users.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+})
+
+var copyEmail = function(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
